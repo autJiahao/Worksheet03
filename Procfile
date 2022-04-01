2333afa9d8a6e5ebcd2467457d9c77b8759cc555
autJiahao <tss4432@aut.ac.nz>
2022-04-01 12:44:04 +0800

    commit message

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: concurrently --kill-others-on-fail \"nodemon app.js\" \"npm run start --prefix mern-app\"
--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // app.js
-
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
 // routes
 const books = require('./routes/api/books');
 
 const app = express();
 
-// Connect Database
-connectDB();
+mongoose
+  .connect(
+    "mongodb+srv://tss4432:Gjh_5922522@cluster0.gs4rx.mongodb.net/Worksheet03?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
+
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -27,6 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8082;
+
 const path = require("path");
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./mern-app/build")));
